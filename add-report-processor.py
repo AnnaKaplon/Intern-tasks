d24c6d373eefe991387cc4bf1a4d31ed422138fb
Anna Kaplon <anna.kaplon19@o2.pl>
2019-02-07 18:21:15 +0100

    adding functions to create memory

--- add-report-processor.py
+++ add-report-processor.py
@@ -1,7 +1,9 @@
-import sys
-import pandas
-import pycountry
 from chardet.universaldetector import UniversalDetector
+import pandas as pd
+import pycountry
+from datetime import datetime
+import math
+import sys
 
 def find_encoding(fileName):
 	"""
@@ -19,6 +21,52 @@ def find_encoding(fileName):
 	detector.close()
 	return detector.result['encoding']
 
+def read_input_report(fileName, enc):
+	df = pd.read_csv(fileName, encoding=enc, names=['date', 'state', 'impression', 'CTR'])
+	
+	for index, row in df.iterrows():
+		date, state, impression = datetime.strptime(row['date'], '%m/%d/%Y'),\
+		row['state'], int(row['impression'])
+		try:
+			CTR = float(row['CTR'])
+		except ValueError:
+			CTR = float(row['CTR'][:-1])
+		try:
+			countryCode = pycountry.subdivisions.lookup(state).country.alpha_3
+		except LookupError:
+			countryCode = 'XXX'
+		add_to_memory(date, countryCode, impression, CTR)
+	
+def add_to_memory(date, countryCode, impression, CTR):
+	if date not in memory:
+		memory[date] = {}
+	if countryCode not in memory[date]:
+		memory[date][countryCode] = {}
+		memory[date][countryCode]['impressions'] = impression
+		memory[date][countryCode]['clicks'] = round(impression*CTR/100)
+	else:
+		memory[date][countryCode]['impressions'] += impression
+		memory[date][countryCode]['clicks'] += round(impression*CTR/100)
+		
+def save_new_report():
+	with open('outputReport.csv', 'w') as file:
+		file.write
+		for date in sorted(memory):
+			for country in sorted(memory[date]):
+				pass
+				
+			
+
+
 if __name__ == '__main__':
 	args = sys.argv
+	global memory 
 	inputFile = args[1]
+	
+	memory = {}
+	try:
+		read_input_report(inputFile, 'utf-8')
+	except UnicodeDecodeError:
+		read_input_report(inputFile, 'utf-16')
+	save_new_report()
+	
