bca3ebb2d03d6b16b12550459f7f523b4388a784
Anna Kaplon <anna.kaplon19@o2.pl>
2019-02-07 21:33:45 +0100

    functions to get content of file

--- web-crawler.py
+++ web-crawler.py
@@ -13,5 +13,28 @@ def site_map(url):
 	else:
 		pass
 
+def get_content(url):
+	soup = get_html(url)
+	title = soup.
+	aTagList = soup.find_all('a', href=True)
+	
+	refs = []
+	for a in aTagList:
+		if basicURL in a['href']:
+			refs.append(a['href'])
+	
+	return title, refs
 
+def get_html(url):
+	"""
+	Function reads html code out of given url, converts it into BeautifulSoup
+	obejct and returns it.
+	
+	Arguments:
+	url - url address
+	"""
+	page = requests.get(url)
+	soup = BeautifulSoup(page.text, 'html.parser')
+	return soup
+	
 	
