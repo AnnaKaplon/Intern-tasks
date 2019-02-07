a578ba4416d9af8889aa81783927f5e66cbf4367
Anna Kaplon <anna.kaplon19@o2.pl>
2019-02-07 21:32:35 +0100

    new file for new task

---
+++ web-crawler.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+def site_map(url):
+	global siteMap
+	global basicURL
+	
+	SiteMap = {}
+	bassicURL = url
+	
+	if url in map:
+		pass
+	else:
+		pass
+
+
+	
