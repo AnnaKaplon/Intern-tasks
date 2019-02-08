191fb12178aa076dd5dd911d324cc9684fb6cb8a
Anna Kaplon <anna.kaplon19@o2.pl>
2019-02-08 17:42:30 +0100

    site_map function modifications

--- web-crawler.py
+++ web-crawler.py
@@ -5,25 +5,39 @@ def site_map(url):
 	global siteMap
 	global basicURL
 	
-	SiteMap = {}
-	bassicURL = url
+	def create_site_map(url):
+		if url in siteMap:
+			pass
+		else:
+			title, refs = get_content(url)
+			add_to_map(url, title, refs)
+			for ref in refs:
+				create_site_map(ref)
 	
-	if url in map:
-		pass
-	else:
-		pass
+	siteMap = {}
+	bassicURL = url
+	create_site_map(url)
+	return siteMap
 
 def get_content(url):
 	soup = get_html(url)
-	title = soup.
+	title = soup.title.string
 	aTagList = soup.find_all('a', href=True)
 	
-	refs = []
+	refs = set()
 	for a in aTagList:
-		if basicURL in a['href']:
-			refs.append(a['href'])
+		ref = a['href']
+		if basicURL in ref:
+			refs.add(ref)
+		elif 'http' not in ref:
+			refs.add(url + ref)
 	
 	return title, refs
+
+def add_to_map(url, title, refs):
+	siteMap[url] = {}
+	siteMap[url]['title'] = title
+	siteMap[url]['links'] = refs
 
 def get_html(url):
 	"""
@@ -37,4 +51,6 @@ def get_html(url):
 	soup = BeautifulSoup(page.text, 'html.parser')
 	return soup
 	
+if __name__ == '__main__':
+	print(site_map('http://0.0.0.0:8000/'))
 	
