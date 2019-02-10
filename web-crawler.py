0ad0521db94596656f152dc492246e8f591ae4b9
Anna Kaplon <anna.kaplon19@o2.pl>
2019-02-10 13:16:31 +0100

    adding documentation

--- web-crawler.py
+++ web-crawler.py
@@ -1,43 +1,62 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import urljoin
+import re
 
-def site_map(url):
-	global siteMap
-	global basicURL
-	
-	def create_site_map(url):
-		if url in siteMap:
-			pass
-		else:
-			title, refs = get_content(url)
-			add_to_map(url, title, refs)
-			for ref in refs:
-				create_site_map(ref)
-	
-	siteMap = {}
-	bassicURL = url
-	create_site_map(url)
+def site_map(url, siteMap={}, basicURL=''):
+	url = delete_bookmark(url)
+	if basicURL == '':
+		basicURL = url
+		
+	title, refs = get_content(url,basicURL)
+	if url not in siteMap:
+		siteMap[url] = {}
+		siteMap[url]['title'] = title
+		siteMap[url]['links'] = refs
+		
+		for ref in refs:
+			siteMap = site_map(ref, siteMap, basicURL)
+			
 	return siteMap
 
-def get_content(url):
+def get_content(url, basicURL):
 	soup = get_html(url)
 	title = soup.title.string
+	return title, set(get_a_refs(soup, url, basicURL) + (get_area_refs(soup, url, basicURL)))
+	
+def get_a_refs(soup, url, basicURL):
 	aTagList = soup.find_all('a', href=True)
 	
-	refs = set()
+	refs = []
 	for a in aTagList:
 		ref = a['href']
-		if basicURL in ref:
-			refs.add(ref)
-		elif 'http' not in ref:
-			refs.add(url + ref)
-	
-	return title, refs
+		if check_correctness(basicURL, ref):
+			refs.append(urljoin(basicURL, delete_bookmark(ref)))
+	return refs
 
-def add_to_map(url, title, refs):
-	siteMap[url] = {}
-	siteMap[url]['title'] = title
-	siteMap[url]['links'] = refs
+def get_area_refs(soup, url, basicURL):
+	areaTagList = soup.find_all('area', href=True)
+	
+	refs = []
+	for area in areaTagList:
+		ref = area['href']
+		if len(soup.find_all('img', usemap='#'+area.parent['name'])) > 0:
+			if check_correctness(basicURL, ref):
+				refs.append(urljoin(basicURL, delete_bookmark(ref)))
+	return refs
+
+def check_correctness(basicURL, ref):
+	if ref.startswith(basicURL):
+		return True
+	elif not ref.startswith('http'):
+		return True
+	return False
+
+def delete_bookmark(url):
+	match = re.search('#.+$', url)
+	if match:
+		return url[:match.start()]
+	return url
 
 def get_html(url):
 	"""
@@ -52,5 +71,5 @@ def get_html(url):
 	return soup
 	
 if __name__ == '__main__':
-	print(site_map('http://0.0.0.0:8000/'))
+	print(site_map('http://0.0.0.0:8000'))
 	
